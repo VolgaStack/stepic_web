dd9519281fa99dec61e12f0d3d8416cfa87e4bd7
Dima <raziel.kdm@gmail.com>
2016-11-13 15:56:45 +0300

    adding wsgi test app

---
+++ hello.py
@@ -0,0 +1,13 @@
+from urlparse import urlparse
+
+def app(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    for key in d:
+        for value in d[key]:
+            data += key + '=' value + '\r\n'
+    
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
