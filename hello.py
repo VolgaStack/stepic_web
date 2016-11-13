dbc93da0e2eb34abdfedc40604476e655f11088c
Dima <raziel.kdm@gmail.com>
2016-11-13 16:10:56 +0300

    gunicorn setup

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ def app(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     for key in d:
         for value in d[key]:
-            data += key + '=' value + '\r\n'
+            data += key + '=' + value + '\r\n'
     
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
